325edccc8c836a63ec726776b5ba7fc5fbcbc2bc
rise <rise.worlds@outlook.com>
2024-12-12 03:57:24 +0000

    first commit

---
+++ programs/server/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <fmt/format.h>
+
+int main(int argc, const char** argv)
+{
+    std::cout << "hello " << std::endl;
+    fmt::print("{}\n", "hello world")
+
+    return 0;
+}
